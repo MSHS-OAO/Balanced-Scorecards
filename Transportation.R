44f3e728c71fe824cc29976292d939af3ba14d97
tommandru <dheeraj.tommandru@mssm.edu>
2022-01-26 17:27:08 -0500

    Fixed transportation script to read only till the previous month

--- Transportation.R
+++ Transportation.R
@@ -185,7 +185,6 @@ process_NPT_raw_data <- function(data){
     mutate(`% of Trips Over 45 Minutes` = round((`No of Trips Over 45 Minutes`)/`No of Transports`,2),
            Month = format(as.Date(paste(month, "01"), "%b %Y %d"), "%m/%d/%Y"),
            Service = "Patient Transport") %>%
-    filter(Month < current_month) %>%
     mutate(Premier_Reporting_Period = format(as.Date(Month, format = "%m/%d/%Y"),"%b %Y"),
            Reporting_Month = format(as.Date(Month, format = "%m/%d/%Y"),"%m-%Y")) %>%
     select(Service,Site,`% of Trips Over 45 Minutes`,`Turnaround Time`,Premier_Reporting_Period,Reporting_Month) %>%
