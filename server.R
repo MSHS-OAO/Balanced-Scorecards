f889dee102bd91bc18f749d66813c14665fa152f
Villegas <armando.villegas@mssm.edu>
2022-05-31 15:44:50 -0400

    remove test code

--- server.R
+++ server.R
@@ -125,8 +125,6 @@ if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=100*1024^2)
                                "Metric_Group",
                                "Metric_Name",
                                "Metric_Name_Submitted"))
-      print("1")
-      print(names(data))
       data <- data %>%
         # Determine status based on status definitions
         mutate(Status = ifelse(is.na(Target), NA,
@@ -144,7 +142,6 @@ if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=100*1024^2)
                                                     "Red", NA))))) %>%
         # Filter on selected reporting period
         filter(Reporting_Month_Ref <= current_period)
-      print("2")
       # Data Period Filtering
       period_filter <- data %>% 
         group_by(Metric_Group,
