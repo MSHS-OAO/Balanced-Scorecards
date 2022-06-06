cb44e0d0d7c18b2accd3968858a2d861b19f033d
Villegas <armando.villegas@mssm.edu>
2022-06-06 15:01:23 -0400

    uncomment saverds for budget data

--- server.R
+++ server.R
@@ -1405,7 +1405,7 @@ if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=100*1024^2)
       budget_process <- budget_raw_file_process(data)
       
       metrics_final_df <<- budget_to_actual_metrics_final_df(budget_process)
-      #saveRDS(metrics_final_df, metrics_final_df_path)
+      saveRDS(metrics_final_df, metrics_final_df_path)
       
       
       budget_to_actual_current_summary_repo <- read_excel(budget_to_actual_path_new)
