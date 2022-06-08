16266c26f203fe12e8f155ea04fcecb808f63125
Tommandru <dheeraj.tommandru@mssm.edu>
2022-06-07 20:22:09 -0400

    Added code to process mdf in Biomed and to Change Treat & Release 90th percentile to make sure the names are consitent in ED and update mdf code

--- biomed.R
+++ biomed.R
@@ -103,38 +103,40 @@ biomed__metrics_final_df_process <- function(data,type){
              Reporting_Month_Ref = Month) %>%
       mutate(Reporting_Month = format(Reporting_Month_Ref,"%m-%Y"),
              Premier_Reporting_Period = format(Reporting_Month_Ref,"%b %Y"),
-             value_rounded = as.numeric(value_rounded))
+             value_rounded = as.numeric(value_rounded))%>%
+      select(-Reporting_Month_Ref)
     
-    summary_metric_filter_subset <- summary_metric_filter %>% select(Metric_Group,Metric_Name,Metric_Name_Submitted)
+    # summary_metric_filter_subset <- summary_metric_filter %>% select(Metric_Group,Metric_Name,Metric_Name_Submitted)
+    # 
+    # metrics_final_df_form <- left_join(metrics_final_df_form,
+    #                                    summary_metric_filter_subset,
+    #                                    by = c("Metric_Name_Submitted"))
+    # metrics_final_df_form <- left_join(metrics_final_df_form,
+    #                                          target_mapping, 
+    #                                          by = c("Service","Site","Metric_Group", "Metric_Name","Metric_Name_Submitted"))
+    # 
+    # metrics_final_df_form <- metrics_final_df_form %>%
+    #   mutate(Variance = between(value_rounded, Range_1, Range_2)) %>%
+    #   filter(Variance %in% c(TRUE,NA))
+    # 
+    # metrics_final_df_form <- metrics_final_df_form[,c("Service",
+    #                                                   "Site",
+    #                                                   "Metric_Group",
+    #                                                   "Metric_Name",
+    #                                                   "Premier_Reporting_Period",
+    #                                                   "Reporting_Month",
+    #                                                   "value_rounded",
+    #                                                   "Target",
+    #                                                   "Status",
+    #                                                   "Reporting_Month_Ref")]
+    # 
+    # updated_rows <- unique(metrics_final_df_form[c("Metric_Name","Reporting_Month","Service", "Site")])
+    # 
+    # metrics_final_df <- anti_join(metrics_final_df, updated_rows)
+    # 
+    # metrics_final_df <- full_join(metrics_final_df,metrics_final_df_form)
     
-    metrics_final_df_form <- left_join(metrics_final_df_form,
-                                       summary_metric_filter_subset,
-                                       by = c("Metric_Name_Submitted"))
-    metrics_final_df_form <- left_join(metrics_final_df_form,
-                                             target_mapping, 
-                                             by = c("Service","Site","Metric_Group", "Metric_Name","Metric_Name_Submitted"))
-    
-    metrics_final_df_form <- metrics_final_df_form %>%
-      mutate(Variance = between(value_rounded, Range_1, Range_2)) %>%
-      filter(Variance %in% c(TRUE,NA))
-    
-    metrics_final_df_form <- metrics_final_df_form[,c("Service",
-                                                      "Site",
-                                                      "Metric_Group",
-                                                      "Metric_Name",
-                                                      "Premier_Reporting_Period",
-                                                      "Reporting_Month",
-                                                      "value_rounded",
-                                                      "Target",
-                                                      "Status",
-                                                      "Reporting_Month_Ref")]
-    
-    updated_rows <- unique(metrics_final_df_form[c("Metric_Name","Reporting_Month","Service", "Site")])
-
-    metrics_final_df <- anti_join(metrics_final_df, updated_rows)
-
-    metrics_final_df <- full_join(metrics_final_df,metrics_final_df_form)
-    
+    metrics_final_df <- metrics_final_df_subset_and_merge(metrics_final_df_form)
     return(metrics_final_df)
 
   }
@@ -145,36 +147,38 @@ biomed__metrics_final_df_process <- function(data,type){
              Reporting_Month_Ref = Month) %>%
       mutate(Reporting_Month = format(Reporting_Month_Ref,"%m-%Y"),
              Premier_Reporting_Period = format(Reporting_Month_Ref,"%b %Y"),
-             Metric_Name_Submitted = "Total Disruptions or Equipment Issues")
+             Metric_Name_Submitted = "Total Disruptions or Equipment Issues")%>%
+      select(-Reporting_Month_Ref)
     
-    summary_metric_filter_subset <- summary_metric_filter %>% 
-      select(Metric_Group,Metric_Name,Metric_Name_Submitted)
+    # summary_metric_filter_subset <- summary_metric_filter %>% 
+    #   select(Metric_Group,Metric_Name,Metric_Name_Submitted)
+    # 
+    # metrics_final_df_form <- left_join(metrics_final_df_form,
+    #                                    summary_metric_filter_subset,
+    #                                    by = c("Metric_Name_Submitted"))
+    # 
+    # metrics_final_df_form <- left_join(metrics_final_df_form,
+    #                                    target_mapping, 
+    #                                    by = c("Service","Site","Metric_Group", "Metric_Name","Metric_Name_Submitted"))
+    # 
+    # metrics_final_df_form <- metrics_final_df_form[,c("Service",
+    #                                                   "Site",
+    #                                                   "Metric_Group",
+    #                                                   "Metric_Name",
+    #                                                   "Premier_Reporting_Period",
+    #                                                   "Reporting_Month",
+    #                                                   "value_rounded",
+    #                                                   "Target",
+    #                                                   "Status",
+    #                                                   "Reporting_Month_Ref")]
+    # 
+    # updated_rows <- unique(metrics_final_df_form[c("Metric_Name","Reporting_Month","Service", "Site")])
+    # 
+    # metrics_final_df <- anti_join(metrics_final_df, updated_rows)
+    # 
+    # metrics_final_df <- full_join(metrics_final_df,metrics_final_df_form)
     
-    metrics_final_df_form <- left_join(metrics_final_df_form,
-                                       summary_metric_filter_subset,
-                                       by = c("Metric_Name_Submitted"))
-    
-    metrics_final_df_form <- left_join(metrics_final_df_form,
-                                       target_mapping, 
-                                       by = c("Service","Site","Metric_Group", "Metric_Name","Metric_Name_Submitted"))
-    
-    metrics_final_df_form <- metrics_final_df_form[,c("Service",
-                                                      "Site",
-                                                      "Metric_Group",
-                                                      "Metric_Name",
-                                                      "Premier_Reporting_Period",
-                                                      "Reporting_Month",
-                                                      "value_rounded",
-                                                      "Target",
-                                                      "Status",
-                                                      "Reporting_Month_Ref")]
-    
-    updated_rows <- unique(metrics_final_df_form[c("Metric_Name","Reporting_Month","Service", "Site")])
-
-    metrics_final_df <- anti_join(metrics_final_df, updated_rows)
-
-    metrics_final_df <- full_join(metrics_final_df,metrics_final_df_form)
-    
+    metrics_final_df <- metrics_final_df_subset_and_merge(metrics_final_df_form)
     return(metrics_final_df)
     
     
