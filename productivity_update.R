54b537053b9a0b0d8819ba541ab5ca392866045f
Villegas <armando.villegas@mssm.edu>
2022-06-21 12:25:17 -0400

    Clean up productivity metrics

--- productivity_update.R
+++ productivity_update.R
@@ -22,4 +22,35 @@ productivity_dept_summary <- function(raw_data){
   
   raw_data <- raw_data %>% select(!`Entity Time Period Desc`)
   
+  # Data Pre-processing -----------------------------------------------------
+  ### Pivot data file longer
+  prod_df <- raw_data %>%
+    mutate(id = seq_len(n())) %>%
+    reshape2::melt(id.var = c("Corporation Code", "Corporation Name", "Entity Code",
+                              "Entity", "Department Reporting Definition ID",
+                              "Department Reporting Definition Name",
+                              "Key Volume", "Mapped Facility Cost Centers", "id"),
+                   na.rm = F) %>%
+    select(-c("id"))
+  
+  ### Process Reporting Period to Reporting Month
+  
+  #Remove dots at end of reporting period
+  prod_df$variable <- gsub("\\..*", "", prod_df$variable)
+  prod_df <- prod_df %>%
+    rename(Premier_Reporting_Period = variable) %>%
+    mutate(Reporting_Month_Start = as.Date(dttm(gsub(" .*$", "",
+                                                     Premier_Reporting_Period)
+    )
+    )
+    )
+  
+  prod_df_final <- prod_df %>% # Fill in Metric name in a column
+    mutate(Metrics = as.numeric(value),
+           Metrics = ifelse(is.na(Metrics), value, ""),
+           Metrics = ifelse(Metrics == "", NA, Metrics)
+    ) %>%
+    fill(Metrics) %>%
+    filter(!is.na(`Corporation Code`)) %>%
+    unique()
 }
