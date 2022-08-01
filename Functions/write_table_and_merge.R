e3da46472515db9296ab4776c090bd83f76c8dc6
Tommandru <dheeraj.tommandru@mssm.edu>
2022-08-01 12:14:41 -0400

    Changed the merge code to DB

--- Functions/write_table_and_merge.R
+++ Functions/write_table_and_merge.R
@@ -13,6 +13,9 @@ library(pool)
 #                  uid = .username,
 #                  pwd = .password)
 con <- dbConnect(odbc::odbc(), "OAO Cloud DB", timeout = 30)
+
+options(odbc.batch_rows = 1000000)
+
 poolcon <- dbPool(drv = odbc::odbc(), 
                   dsn = "OAO Cloud DB")
 
@@ -49,11 +52,11 @@ get_values <- function(x,table_name){
 write_temporary_table_to_database_and_merge <- function(processed_input_data,table_name){
   
   # Constants
-  DATA_TYPES <- c(SERVICE = "Varchar2(10 CHAR)",
+  DATA_TYPES <- c(SERVICE = "Varchar2(100 CHAR)",
                   SITE = "Varchar2(10 CHAR)",
                   REPORTING_MONTH = "DATE",
-                  METRIC_NAME_SUBMITTED = "Varchar2(20 CHAR)",
-                  VALUE = "numeric(10,10)",
+                  METRIC_NAME_SUBMITTED = "Varchar2(200 CHAR)",
+                  VALUE = "numeric(30,10)",
                   PREMIER_REPORTING_PERIOD = "Varchar2(20 CHAR)",
                   UPDATED_TIME = "Timestamp",
                   UPDATED_USER = "Varchar2(100 CHAR)" )
@@ -62,8 +65,13 @@ write_temporary_table_to_database_and_merge <- function(processed_input_data,tab
   
   
   # Add UPDATE_TIME 
+  
+  # check for all the fields are characters
+  
   processed_input_data <- processed_input_data %>%
-    mutate(UPDATED_TIME = as.character(Sys.time()))%>%
+    mutate(UPDATED_TIME = as.character(Sys.time()),
+           VALUE = as.character(VALUE),
+           VALUE = coalesce(VALUE,"NULL"))%>%
     select(SERVICE, 
            SITE, 
            REPORTING_MONTH,
@@ -76,6 +84,8 @@ write_temporary_table_to_database_and_merge <- function(processed_input_data,tab
            # UPDATED_USER = NA_character_) %>%
     # rename(METRIC_NAME_SUBMITTED = Metric,
     #        VALUE=Number)
+  
+  view(processed_input_data)
   
   inserts <- lapply(
     lapply(
@@ -91,37 +101,40 @@ write_temporary_table_to_database_and_merge <- function(processed_input_data,tab
                       {values}
                     SELECT 1 from DUAL;')
   
+  print(all_data)
+  
   query = glue('MERGE INTO SUMMARY_REPO SR
                   USING "{TABLE_NAME}" SOURCE_TABLE
                   ON (  SR."SITE" = SOURCE_TABLE."SITE" AND
-                        SR."MONTH" = SOURCE_TABLE."MONTH" AND
+                        SR."REPORTING_MONTH" = SOURCE_TABLE."REPORTING_MONTH" AND
                         SR."SERVICE" = SOURCE_TABLE."SERVICE" AND
                         SR."METRIC_NAME_SUBMITTED" = SOURCE_TABLE."METRIC_NAME_SUBMITTED")
                   WHEN MATCHED THEN 
                   UPDATE  SET SR."VALUE" = SOURCE_TABLE."VALUE",
                               SR."UPDATED_TIME" = SOURCE_TABLE."UPDATED_TIME",
-                              SR."PREMIER_REPORTING_PERIOD" = SOURCE_TABLE."PREMIER_REPORTING_PERIOD",
-                              SR."UPDATED_USER" = SOURCE_TABLE."UPDATED_USER"
+                              SR."UPDATED_USER" = SOURCE_TABLE."UPDATED_USER",
+                              SR."PREMIER_REPORTING_PERIOD" = SOURCE_TABLE."PREMIER_REPORTING_PERIOD"
                   WHEN NOT MATCHED THEN
                   INSERT( SR."SITE",
-                          SR."MONTH",
+                          SR."REPORTING_MONTH",
                           SR."SERVICE",
                           SR."METRIC_NAME_SUBMITTED",
                           SR."VALUE", 
                           SR."UPDATED_TIME", 
-                          SR."PREMIER_REPORTING_PERIOD",
-                          SR."UPDATED_USER")  
+                          SR."UPDATED_USER",
+                          SR."PREMIER_REPORTING_PERIOD")  
                   VALUES( SOURCE_TABLE."SITE",
-                          SOURCE_TABLE."MONTH",
+                          SOURCE_TABLE."REPORTING_MONTH",
                           SOURCE_TABLE."SERVICE",
                           SOURCE_TABLE."METRIC_NAME_SUBMITTED",
                           SOURCE_TABLE."VALUE", 
                           SOURCE_TABLE."UPDATED_TIME",
-                          SOURCE_TABLE."PREMIER_REPORTING_PERIOD",
-                          SOURCE_TABLE."UPDATED_USER");')
+                          SOURCE_TABLE."UPDATED_USER",
+                          SOURCE_TABLE."PREMIER_REPORTING_PERIOD");')
   
   drop_query <- glue('DROP TABLE "{TABLE_NAME}";')
-
+  
+  print(drop_query)
   
   dbCreateTable(con,
                 TABLE_NAME,
@@ -152,7 +165,15 @@ write_temporary_table_to_database_and_merge <- function(processed_input_data,tab
 # data<- write_temporary_table_to_database_and_merge(summary_repo_lab_tat1,"SR3")
 
 
-
+# EVS <- read_excel(paste0(home_path,"Summary Repos for Database/TAT - EVS.xlsx"))
+# Nursing <- read_excel(paste0(home_path,"Summary Repos for Database/Nursing.xlsx"))
+# 
+# data<- write_temporary_table_to_database_and_merge(ImagingDR,"ImagingDR")
+# data<- write_temporary_table_to_database_and_merge(ImagingIR,"ImagingIR")
+# data<- write_temporary_table_to_database_and_merge(BiomedKPIS,"BiomedKPIS")
+# data<- write_temporary_table_to_database_and_merge(BiomedDI,"BiomedDI")
+# data<- write_temporary_table_to_database_and_merge(EVS,"EVS")
+# data<- write_temporary_table_to_database_and_merge(Nursing,"Nursing")
 
 
 # data<- write_temporary_table_to_database_and_merge(processed_sunquest_may_data,"SUNQUEST")
