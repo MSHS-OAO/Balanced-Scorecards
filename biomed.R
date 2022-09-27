ad7582c2402f9c8a95d1c5196832cba158181774
Tommandru <dheeraj.tommandru@mssm.edu>
2022-09-27 11:04:13 -0400

    Commented the flat file processing code in biomed.R

--- biomed.R
+++ biomed.R
@@ -2,48 +2,48 @@
 
 # Disruptions and Issues Reports -------------
 # Import historical summary
-disruptions_issues_reports <- read_excel(bmedi_table_path)
-
-# Reformat "Month" and "Date" column in Disruptions and Issues for merging
-disruptions_issues_reports <- disruptions_issues_reports %>%
-  mutate(Month = date(Month))
-
-# Determine last month and next month for Security Incident Reports
-biomedDI_last_month <- max(disruptions_issues_reports$Month)
-
-# Transform data to UI form
-disruptions_issues_reports_ui <- disruptions_issues_reports %>%
-  select(-Service) %>%
-  filter(Month >= biomedDI_last_month - months(7)) %>%
-  mutate(Month = format(Month, "%b-%Y")) %>%
-  pivot_wider(names_from = "Month",values_from = `Total Disruptions/Issues`,values_fill=NA_integer_) %>%
-  mutate(Metric = "Total Disruptions and Issues") %>%
-  relocate(Metric,.after=Site) #%>%
-  # mutate('{format(biomedDI_last_month + months(1), "%b-%Y")}' := "")
-
-
-# KPIs Biomed -------------
-# Import historical summary
-kpibme_reports <- read_excel(bmekpi_table_path)
-kpibme_reports$Number <- as.numeric(kpibme_reports$Number)
-
-# Reformat "Month" column in KPIs for merging
-kpibme_reports <- kpibme_reports %>%
-  mutate(Month = date(Month))
-
-# Determine last month and next month for KPIs
-kpibme_last_month <- max(kpibme_reports$Month)
-
-# Transform data to UI form
-kpibme_reports_ui <- kpibme_reports %>%
-  select(-Service) %>%
-  filter(Month >= kpibme_last_month - months(7)) %>%
-  mutate(Month = format(Month, "%b-%Y"),
-         Number = as.numeric(Number)) %>%
-  pivot_wider(names_from = "Month",values_from = Number,values_fill=NA_integer_) %>%
-  group_by(Site) %>%
-  # mutate('{format(kpibme_last_month + months(1), "%b-%Y")}' := NA) %>%
-  arrange(Site)
+# disruptions_issues_reports <- read_excel(bmedi_table_path)
+# 
+# # Reformat "Month" and "Date" column in Disruptions and Issues for merging
+# disruptions_issues_reports <- disruptions_issues_reports %>%
+#   mutate(Month = date(Month))
+# 
+# # Determine last month and next month for Security Incident Reports
+# biomedDI_last_month <- max(disruptions_issues_reports$Month)
+# 
+# # Transform data to UI form
+# disruptions_issues_reports_ui <- disruptions_issues_reports %>%
+#   select(-Service) %>%
+#   filter(Month >= biomedDI_last_month - months(7)) %>%
+#   mutate(Month = format(Month, "%b-%Y")) %>%
+#   pivot_wider(names_from = "Month",values_from = `Total Disruptions/Issues`,values_fill=NA_integer_) %>%
+#   mutate(Metric = "Total Disruptions and Issues") %>%
+#   relocate(Metric,.after=Site) #%>%
+#   # mutate('{format(biomedDI_last_month + months(1), "%b-%Y")}' := "")
+# 
+# 
+# # KPIs Biomed -------------
+# # Import historical summary
+# kpibme_reports <- read_excel(bmekpi_table_path)
+# kpibme_reports$Number <- as.numeric(kpibme_reports$Number)
+# 
+# # Reformat "Month" column in KPIs for merging
+# kpibme_reports <- kpibme_reports %>%
+#   mutate(Month = date(Month))
+# 
+# # Determine last month and next month for KPIs
+# kpibme_last_month <- max(kpibme_reports$Month)
+# 
+# # Transform data to UI form
+# kpibme_reports_ui <- kpibme_reports %>%
+#   select(-Service) %>%
+#   filter(Month >= kpibme_last_month - months(7)) %>%
+#   mutate(Month = format(Month, "%b-%Y"),
+#          Number = as.numeric(Number)) %>%
+#   pivot_wider(names_from = "Month",values_from = Number,values_fill=NA_integer_) %>%
+#   group_by(Site) %>%
+#   # mutate('{format(kpibme_last_month + months(1), "%b-%Y")}' := NA) %>%
+#   arrange(Site)
 
 # function to append the new data to summary repo- KPIs & Disruptions and Issues -----
 process_manual_entry_to_summary_repo_format_biomed <- function(data,type,updated_user){
