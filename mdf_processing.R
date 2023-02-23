3c0e8bb7c1255b81681c2e93a2ddd992a7719de1
armando-v <>
2023-02-22 20:06:43 -0500

    add budget processing

--- mdf_processing.R
+++ mdf_processing.R
@@ -1,10 +1,22 @@
 library(dplyr)
 library(readxl)
+
 mdf <- readRDS("mdf_pull_2_8.rds")
 mapping_file <- read_csv("BSC_MAPPING_TABLE.csv")
+budget_metrics <- c("Budget to Actual Variance - Total", "Budget to Actual Variance - Labor", "Budget to Actual Variance - Non Labor")
+budget_file <- read_excel("budget_repo_2_22.xlsx")
+
+budget_monthly <- budget_file %>% select(-Value_ytd) %>% mutate(Metric_Name_Submitted = paste0(Metric_Name_Submitted, " (Monthly)"))
+budget_ytd <- budget_file %>% select(-Value) %>% mutate(Metric_Name_Submitted = paste0(Metric_Name_Submitted, " (YTD)")) %>%
+                rename(Value = Value_ytd)
+budget_complete <- bind_rows(budget_monthly, budget_ytd)
 
 mdf <- left_join(mdf, mapping_file[c("SERVICE", "METRIC_NAME_SUBMITTED", "METRIC_NAME")], by = c("Service" = "SERVICE",
                                                                    "Metric_Name" = "METRIC_NAME"))
+mdf <- mdf %>% filter(!(METRIC_NAME_SUBMITTED %in% budget_metrics))
+mdf <- bind_rows(mdf, budget_complete)
+
+
 
 mdf <- mdf %>% mutate(Premier_Reporting_Period = ifelse(Metric_Group == "Premier", Premier_Reporting_Period, format(Reporting_Month_Ref, "%b %Y"))) %>% 
   select(-Metric_Group, -Month, -Reporting_Month, -Metric_Name) %>% rename(Reporting_Month = Reporting_Month_Ref, value = value_rounded) %>%
