621e0185b7aa5856a261c34181d7a6c361388b97
armando-v <>
2023-02-22 19:51:17 -0500

    update mdf processing to inlcude chnages to Imaging and EVS

--- mdf_processing.R
+++ mdf_processing.R
@@ -1,7 +1,20 @@
+library(dplyr)
+library(readxl)
 mdf <- readRDS("mdf_pull_2_8.rds")
+mapping_file <- read_csv("BSC_MAPPING_TABLE.csv")
+
+mdf <- left_join(mdf, mapping_file[c("SERVICE", "METRIC_NAME_SUBMITTED", "METRIC_NAME")], by = c("Service" = "SERVICE",
+                                                                   "Metric_Name" = "METRIC_NAME"))
 
 mdf <- mdf %>% mutate(Premier_Reporting_Period = ifelse(Metric_Group == "Premier", Premier_Reporting_Period, format(Reporting_Month_Ref, "%b %Y"))) %>% 
-  select(-Metric_Group, -Month, -Reporting_Month) %>% rename(Reporting_Month = Reporting_Month_Ref, value = value_rounded)
+  select(-Metric_Group, -Month, -Reporting_Month, -Metric_Name) %>% rename(Reporting_Month = Reporting_Month_Ref, value = value_rounded) %>%
+  mutate(METRIC_NAME_SUBMITTED = ifelse(METRIC_NAME_SUBMITTED == 'Non-IsolationAverage TAT' &
+                                            Service == 'Environmental Services', 'Non-Isolation Average TAT', METRIC_NAME_SUBMITTED)) %>%
+  mutate(METRIC_NAME_SUBMITTED = ifelse(METRIC_NAME_SUBMITTED == 'Prime Time Room Utilization ESTIMATE All Rooms (%)' &
+                                        Service == 'Imaging', 'Prime Time Room Utilization ESTIMATE All Rooms (%) 8:30A-5:00P, M-F', METRIC_NAME_SUBMITTED)
+         )
+
+
 
 time_updated <- read_excel("time_updated_2_8.xlsx")
 time_updated <- time_updated %>% group_by(Service) %>% filter(Updated == max(Updated))
