165e6f3d2b3d2e760d9f6333ffc97422731b85ea
Dheeraj Tommandru <tommad01@mssm.edu>
2023-03-31 16:20:22 -0400

    Adding Clinical Nutrition Data processing function

---
+++ ClinicalNurtrition.R
@@ -0,0 +1,16 @@
+cn_dept_summary <- function(raw_data,updated_user){
+  
+  processed_data <- raw_data %>%
+    pivot_longer(-c("Site", "Metric"),names_to = "REPORTING_MONTH", values_to = "Value")
+  processed_data <- processed_data %>% 
+    mutate(REPORTING_MONTH = as.Date(as.integer(REPORTING_MONTH), origin = "1899-12-30"),
+           SERVICE = 'Clinical Nutrition',
+           UPDATED_USER = updated_user,
+           PREMIER_REPORTING_PERIOD = format(REPORTING_MONTH,"%b %Y")) %>%
+    rename(`SITE` = `Site`,
+           `VALUE` = `Value`,
+           `METRIC_NAME_SUBMITTED` = `Metric`) %>%
+    drop_na() %>%
+    select(SERVICE,SITE,REPORTING_MONTH,PREMIER_REPORTING_PERIOD,METRIC_NAME_SUBMITTED,VALUE,UPDATED_USER)
+  
+}
