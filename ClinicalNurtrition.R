3c4c9c470d34c671ae418db2ba40f5b05bb94dcd
Dheeraj Tommandru <tommad01@mssm.edu>
2023-04-03 14:07:20 -0400

    Clinical Nutrition Changes

--- ClinicalNurtrition.R
+++ ClinicalNurtrition.R
@@ -6,7 +6,11 @@ cn_dept_summary <- function(raw_data,updated_user){
     mutate(REPORTING_MONTH = as.Date(as.integer(REPORTING_MONTH), origin = "1899-12-30"),
            SERVICE = 'Clinical Nutrition',
            UPDATED_USER = updated_user,
-           PREMIER_REPORTING_PERIOD = format(REPORTING_MONTH,"%b %Y")) %>%
+           PREMIER_REPORTING_PERIOD = format(REPORTING_MONTH,"%b %Y"),
+           VALUE = case_when(Metric  == "RD pended orders"~ VALUE/100,
+                             Metric  == "Nutrition assessment for patient w/ PI"~ VALUE/100,
+                             Metric  == "Malnutrition Identification"~ VALUE/100,
+                             Metric  == "Malnutrition Revenue"~ VALUE)) %>%
     rename(`SITE` = `Site`,
            `METRIC_NAME_SUBMITTED` = `Metric`) %>%
     drop_na() %>%
