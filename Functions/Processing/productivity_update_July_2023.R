4ff01d059d982e6927de3caaecd44c07a77f1c20
armando-v <>
2023-07-03 15:30:17 -0400

    remove over time percent of paid hours for clinical nutrition in MSBI, MSH, MSM, MSQ, MSW

--- Functions/Processing/productivity_update_July_2023.R
+++ Functions/Processing/productivity_update_July_2023.R
@@ -225,8 +225,15 @@ productivity_processing <- function(raw_data, updated_user) {
   if(ytd_flag == 1) {
     prod_df_aggregate <- prod_df_aggregate %>% mutate(METRIC_NAME_SUBMITTED = paste0(METRIC_NAME_SUBMITTED, " (FYTD)"))
   }
+  
+  prod_df_aggregate_cn <- prod_df_aggregate %>% filter(SERVICE == "Clinical Nutrition" & METRIC_NAME_SUBMITTED == "Overtime Percent of Paid Hours") %>% filter(SITE %in% c("MSB", "MSW"))
+  prod_df_aggregate <- prod_df_aggregate %>% filter(SERVICE != "Clinical Nutrition" & METRIC_NAME_SUBMITTED != "Overtime Percent of Paid Hours")
+  
+  prod_df_aggregate <- rbind(prod_df_aggregate, prod_df_aggregate_cn)
+  
   prod_df_aggregate <- prod_df_aggregate %>% filter(VALUE != "NaN")
   
   
+
   
 }
