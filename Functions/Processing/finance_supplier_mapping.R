6acf6ec3e176cb455ad3695574e17fc6e36887b3
Dheeraj Tommandru <tommad01@mssm.edu>
2024-05-21 11:01:27 -0400

    Fixed the ORA-30926

--- Functions/Processing/finance_supplier_mapping.R
+++ Functions/Processing/finance_supplier_mapping.R
@@ -1,12 +1,13 @@
-data <- read.xlsx("/SharedDrive//deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/Balanced Scorecards Automation/Data_Dashboard/Finance Backend/Supply category to Sub-account mapping.xlsx")
-updated_user = "Laith Test"
+# data <- read.xlsx("/SharedDrive//deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/Balanced Scorecards Automation/Data_Dashboard/Finance Backend/Supply category to Sub-account mapping.xlsx")
+# updated_user = "Laith Test"
 
 
 process_finance_supplier_mapping_data <-  function(data, updated_user) {
  
   data <- data %>%
     rename(CATEGORY = Category) %>%
-    mutate(UPDATED_USER = updated_user)
+    mutate(UPDATED_USER = updated_user)%>%
+    distinct()
   
  
   write_temporary_table_to_database_and_merge_updated(data,
@@ -17,4 +18,4 @@ process_finance_supplier_mapping_data <-  function(data, updated_user) {
   
 }
 
-process_finance_supplier_mapping_data(data, updated_user)
+# process_finance_supplier_mapping_data(data, updated_user)
