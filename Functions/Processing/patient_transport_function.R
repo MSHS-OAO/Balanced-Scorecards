dc6118df15022327b995e9ef0af51b7e87d9231d
LaithKAzzam <laith.alazzam@mssm.edu>
2024-10-08 12:27:40 -0400

    Updated Patient Transport

--- Functions/Processing/patient_transport_function.R
+++ Functions/Processing/patient_transport_function.R
@@ -2,17 +2,17 @@ library(dplyr)
 library(magrittr)
 library(lubridate)
 
-# data <- read.csv("/SharedDrive//deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/Balanced Scorecards Automation/Data_Dashboard/Finance Backend/Patient Transport Jan-Apr 2024 data.csv")
-# updated_user = "Laith Test"
+data <- read.xlsx("~/Balanced-Scorecards/Test/PT Transport All Data - System Template.xlsx")
+updated_user = "Laith Test"
 
 
 process_patient_transport_data <-  function(data, updated_user) {
   
-  data <- data %>%
+  data2 <- data %>%
     mutate(SERVICE = "Patient & Equipment Transport") %>%
     mutate(UPDATED_USER = updated_user) %>%
-    mutate(REPORTING_MONTH = as.Date(REPORTING_MONTH, format = "%Y-%m-%d")) %>%
-    mutate(PREMIER_REPORTING_PERIOD = format(REPORTING_MONTH,"%b %Y")) %>%
+    mutate(REPORTING_MONTH = as.Date(REPORTING_MONTH, origin = "1899-12-30")) %>%
+    mutate(PREMIER_REPORTING_PERIOD = format(REPORTING_MONTH,"%b-%Y")) %>%
     mutate(VALUE = round(VALUE, 3)) %>%
     rename(METRIC_NAME_SUBMITTED=METRIC_NAME_SUMMARY) %>%
     select(SERVICE,SITE,REPORTING_MONTH,METRIC_NAME_SUBMITTED,VALUE,UPDATED_USER,PREMIER_REPORTING_PERIOD) 
