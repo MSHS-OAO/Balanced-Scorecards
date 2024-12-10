d4ca2a40bea21ec121d03957c4aab72f654267e5
Dheeraj Tommandru <tommad01@mssm.edu>
2024-12-10 12:03:13 -0500

    Added SQL Queries

--- server.R
+++ server.R
@@ -1796,9 +1796,9 @@ if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=100*1024^2)
       
 
       
-      service_input <- 'Biomed / Clinical Engineering'
-      month_input <- "10-2024"
-      site_input <- "MSH"
+      # service_input <- 'Biomed / Clinical Engineering'
+      # month_input <- "10-2024"
+      # site_input <- "MSH"
       
       
       # Get the data from data base
