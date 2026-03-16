108e76b63852726ca40ea295af92e29f09164416
Dheeraj Tommandru <tommad01@mssm.edu>
2026-03-16 09:12:59 -0400

    Changes to get budget function

--- Functions/get_budget_data.R
+++ Functions/get_budget_data.R
@@ -36,6 +36,39 @@ get_budget_data <- function(service,month_input){
                                        "Metric_Name_Submitted")) %>%
     mutate(Month = as.Date(Month, format = "%m-%Y-%d"))
   
+  if(service == "Nursing") {
+    budget_data_labor_non_labor <- tbl(conn, "BSC_FINANCE_TABLE_VIEW") %>%
+      filter(FUNCTION == service, 
+             TO_DATE(min_month, format) <= MONTH) %>%
+      collect() %>%
+      mutate(SITE = case_when(
+        SITE == 'MSH' ~ "MSH",
+        SITE == 'MS STL' ~ "MSM",
+        SITE == 'MS WEST' ~ "MSW",
+        SITE == 'MS BIB' ~ "MSB",
+        SITE == 'MSBHC' ~ "MSBHC",
+        SITE == 'MS BI' ~ "MSBI",
+        SITE == 'MS NYEE' ~ "NYEE",
+        TRUE ~ SITE # The TRUE ~ condition serves as the "else" or default
+      )) %>%
+      select(SITE, FUNCTION,EXPTYPE,MONTH,SUM_OF_MONTH_BUDGET, SUM_OF_YTD_BUDGET) %>%
+      group_by(SITE, MONTH, FUNCTION,EXPTYPE) %>%
+      summarise(Value= sum(SUM_OF_MONTH_BUDGET),
+                Value_ytd = sum(SUM_OF_YTD_BUDGET))%>%
+      rename(Metric_Name_Submitted = EXPTYPE,
+             Site = SITE,
+             Month = MONTH,
+             Service = FUNCTION) %>%
+      mutate(Metric_Name_Submitted = case_when(
+        Metric_Name_Submitted == 'Salaries' ~ "Budget_Total_Labor",
+        Metric_Name_Submitted == 'Supplies' ~ "Budget_Total_Non_Labor",
+        TRUE ~ Metric_Name_Submitted # The TRUE ~ condition serves as the "else" or default
+      ))
+    budget_data_repo_final <- rbind(budget_data_repo_final,
+                                    budget_data_labor_non_labor)
+  }
+    
+  
   dbDisconnect(conn)
   
   return(budget_data_repo_final)
